770c3cc5144b5a5232a829419381ad33174993eb
Rafael Padilha <rafaspadilha@gmail.com>
2022-03-11 11:30:11 -0300

    IV.F - Sens Analysis Geo Location

---
+++ paper_evaluation/IV.F_sensitivity_geographic_location/README.md
@@ -0,0 +1,5 @@
+# IV.F - Sensitivity Analysis: Geographic Location
+
+Files implementing the addition of noise to the location coordinates. 
+- `dataLoader.py`: extends the DataLoader class to generate testing samples with location noise and training samples with location augmentation;
+- `testWithLocError.py`: testing script to evaluate the test dataset with some noise degree;
--- paper_evaluation/IV.F_sensitivity_geographic_location/dataLoader.py
+++ paper_evaluation/IV.F_sensitivity_geographic_location/dataLoader.py
@@ -19,12 +19,122 @@ from dataLoader import *
 
 
 
+def randomPlusOrMinus():
+    return 1 if random.random() < 0.5 else -1
+
+
+
+
 class DataLoaderWithLocError(DataLoader):
     def __init__(self, setToLoad="train", includeSatellite=True, outputTransientAttributes=True):
         DataLoader.__init__(self, setToLoad,
                             includeLocation=True,
                             includeSatellite=includeSatellite,
                             outputTransientAttributes=outputTransientAttributes)
+
+
+
+
+
+
+
+
+    #### Method used to generate batches of data with Location Augmentation
+    def loadImagesInBatchesWithLocAug(self, batchSize):
+        # Initialize arrays to store each input modality and labels
+        inputBatch, outputBatch = [], []
+        gBatch, aBatch, tBatch, lBatch, labels, transAtt = [], [], [], [], [], []
+
+        # Count the number of samples in the batch
+        nInBatch = 0
+
+        ### Epoch loop
+        while 1:
+            ## Shuffle data at each epoch
+            rndIdx = np.arange(self.nPairsInSet)
+            np.random.shuffle(rndIdx)
+
+            for idx in rndIdx:
+
+                # Load and preprocess ground-level image
+                try:
+                    gImg = load_preprocess_groundImg(self.groundPaths[idx])
+                except (OSError, IOError) as e:
+                    #If error in loading, go to the next sample
+                    continue
+
+                # Load and preprocess satellite image
+                if self.includeSatellite:
+                    try:
+                        aImg = load_preprocess_aerialImg(self.aerialPaths[idx])
+                    except (OSError, IOError) as e:
+                        #If error in loading, go to the next sample
+                        continue
+
+                # Include each image in the batch twice (consistent and inconsistent)
+                gBatch += [gImg, gImg]
+
+                if self.includeSatellite:
+                    aBatch += [aImg, aImg]
+
+
+
+
+
+                #Augment location
+                originalLoc = np.array(self.locLabels[idx])
+
+                if random.choice([0, 1]) == 1:
+                    errorPercentage = random.choice(
+                        [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4])
+                    originalLoc[0] *= 1 + (errorPercentage * randomPlusOrMinus())
+                    originalLoc[0] = max(-90.0, min(originalLoc[0], 90.0))
+
+                if random.choice([0, 1]) == 1:
+                    errorPercentage = random.choice(
+                        [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4])
+                    originalLoc[1] *= 1 + (errorPercentage * randomPlusOrMinus())
+                    originalLoc[1] = max(-180.0, min(originalLoc[1], 180.0))
+
+                #Add the location information to the batch
+                loc = preprocess_loc(originalLoc)
+                lBatch += [loc, loc]
+
+
+
+                #Process time info and tamper the time for one pair
+                time = preprocess_time(self.timeLabels[idx])
+                fakeTime = self.fakeTime(self.timeLabels[idx])
+                tBatch += [time, fakeTime]
+
+                #Label 0 = real/consistent tuple
+                #Label 1 = tampered/inconsistent tuple
+                labels += [to_categorical(0, num_classes=2),
+                           to_categorical(1, num_classes=2)]
+
+                # Add the transient attributes to the output
+                if self.outputTA:
+                    transAtt += [self.transientAttributes[idx],
+                                 self.transientAttributes[idx]]
+
+                nInBatch += 2
+                if nInBatch >= batchSize:
+                    inputBatch = [np.array(gBatch)]
+                    inputBatch += [np.array(aBatch)
+                                   ] if self.includeSatellite else []
+                    inputBatch += [np.array(lBatch)
+                                   ] if self.includeLocation else []
+                    inputBatch += [np.array(tBatch)]
+
+                    outputBatch = [np.array(labels)]
+                    outputBatch += [np.array(transAtt),
+                                    np.array(transAtt)] if self.outputTA else []
+
+                    yield inputBatch, outputBatch
+                    gBatch, aBatch, tBatch, lBatch, labels, transAtt = [], [], [], [], [], []
+                    inputBatch, outputBatch = [], []
+                    nInBatch = 0
+
 
 
 
